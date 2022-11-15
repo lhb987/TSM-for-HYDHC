773871bb30db369c95887e7166a5a267956544f8
ggavy_MAC <sunutf@gmail.com>
2022-11-15 20:10:28 +0900

    change resume path

--- scripts/test_tsm_dhc_rgb_80f.sh
+++ scripts/test_tsm_dhc_rgb_80f.sh
@@ -1,3 +1,6 @@
-python main.py \
-    --resume=pretrained/TSM_kinetics_RGB_resnet50_shift8_blockres_avg_segment8_e50.pth \
-    --evaluate
+python main.py DHC RGB \
+     --evaluate --resume 'checkpoint/ckpt.best.pth.tar' \
+     --arch resnet50 --num_segments 80 \
+     --gd 20 --lr 0.00125 --lr_steps 10 20 --epochs 25 \
+     --batch-size 8 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
+     --shift --shift_div=8 --shift_place=blockres --npb
